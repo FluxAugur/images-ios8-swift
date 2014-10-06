6573c0013aed997ab26b76053cfbbc3226c6ab76
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-10-05 21:24:00 -0400

    prepped for GitHub push

--- Images/ViewController.swift
+++ Images/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var counter = 1
+  
+  @IBOutlet weak var alien: UIImageView!
+  
+  @IBAction func buttonPressed(sender: AnyObject) {
+    counter++
+    
+    if (counter == 6) {
+      counter = 1
+    }
+    
+    alien.image = UIImage(named: "alien\(counter).png")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,16 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  override func viewDidLayoutSubviews() {
+    alien.center = CGPointMake(alien.center.x - 400, alien.center.y)
+    alien.alpha = 0
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    UIView.animateWithDuration(1, animations: {
+      self.alien.center = CGPointMake(self.alien.center.x + 400, self.alien.center.y)
+    })
+    self.alien.alpha = 1
+  }
 }
 
